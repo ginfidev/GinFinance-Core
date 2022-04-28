87cef17f13d7c1b613d6653799a7ad918b2e89db
ginfidev <dev@gin.finance>
2022-04-28 10:03:37 -0400

    Add more fee setter

--- GinFinanceFactory.sol
+++ GinFinanceFactory.sol
@@ -11,6 +11,10 @@ contract GinFinanceFactory is IGinFinanceFactory {
 
     address public override feeTo;
     address public override feeToSetter;
+    address public override feeToAllocSetter;
+    address public override defaultFeeSetter;
+    address public override pairFeeSetter;
+
     uint public override feeToAlloc = 5;
     uint public override defaultFee = 30;
     uint public constant override FEE_DENOMINATOR = 10000;
@@ -20,14 +24,20 @@ contract GinFinanceFactory is IGinFinanceFactory {
     address[] public override allPairs;
 
     event PairCreated(address indexed token0, address indexed token1, address pair, uint);
+    event SetPairFeeSetter(address pairFeeSetter);
     event SetPairFee(address indexed pair, uint pairFee);
+    event SetDefaultFeeSetter(address defaultFeeSetter);
     event SetDefaultFee(uint defaultFee);
+    event SetFeeToAllocSetter(address feeToAllocSetter);
     event SetFeeToAlloc(uint feeToAlloc);
     event SetFeeToSetter(address feeToSetter);
     event SetFeeTo(address feeTo);
 
-    constructor(address _feeToSetter) public {
+    constructor(address _feeToSetter, address _feeToAllocSetter, address _defaultFeeSetter, address _pairFeeSetter) public {
         feeToSetter = _feeToSetter;
+        feeToAllocSetter = _feeToAllocSetter;
+        defaultFeeSetter = _defaultFeeSetter;
+        pairFeeSetter = _pairFeeSetter;
     }
 
     function pairCodeHash() external pure returns (bytes32) {
@@ -78,6 +88,12 @@ contract GinFinanceFactory is IGinFinanceFactory {
         emit SetFeeToAlloc(_feeToAlloc);
     }
 
+    function setFeeToAllocSetter(address _feeToAllocSetter) external override {
+        require(msg.sender == feeToAllocSetter, 'GinFinanceFactory: FORBIDDEN');
+        feeToAllocSetter = _feeToAllocSetter;
+        emit SetFeeToAllocSetter(_feeToAllocSetter);
+    }
+
     // Max is 1%
     function setDefaultFee(uint _defaultFee) external override {
         require(msg.sender == feeToSetter, 'GinFinanceFactory: FORBIDDEN');
@@ -86,12 +102,24 @@ contract GinFinanceFactory is IGinFinanceFactory {
         emit SetDefaultFee(_defaultFee);
     }
 
+    function setDefaultFeeSetter(address _defaultFeeSetter) external override {
+        require(msg.sender == defaultFeeSetter, 'GinFinanceFactory: FORBIDDEN');
+        defaultFeeSetter = _defaultFeeSetter;
+        emit SetDefaultFeeSetter(_defaultFeeSetter);
+    }
+
     // Max is 1%
     function setPairFee(address _pair, uint _fee) external override {
         require(msg.sender == feeToSetter, 'GinFinanceFactory: FORBIDDEN');
         require(_fee != 0 && _fee <= 100, 'GinFinanceFactory: FEE_OUT_OF_RANGE');
         pairFee[_pair] = _fee;
         emit SetPairFee(_pair, _fee);
+    }
+
+    function setPairFeeSetter(address _pairFeeSetter) external override {
+        require(msg.sender == pairFeeSetter, 'GinFinanceFactory: FORBIDDEN');
+        pairFeeSetter = _pairFeeSetter;
+        emit SetPairFeeSetter(_pairFeeSetter);
     }
 }
 
--- interfaces/IGinFinanceFactory.sol
+++ interfaces/IGinFinanceFactory.sol
@@ -4,8 +4,11 @@ pragma solidity 0.6.12;
 
 interface IGinFinanceFactory {
     event PairCreated(address indexed token0, address indexed token1, address pair, uint);
+    event SetPairFeeSetter(address pairFeeSetter);
     event SetPairFee(address indexed pair, uint pairFee);
+    event SetDefaultFeeSetter(address defaultFeeSetter);
     event SetDefaultFee(uint defaultFee);
+    event SetFeeToAllocSetter(address feeToAllocSetter);
     event SetFeeToAlloc(uint feeToAlloc);
     event SetFeeToSetter(address feeToSetter);
     event SetFeeTo(address feeTo);
@@ -13,9 +16,12 @@ interface IGinFinanceFactory {
     function feeTo() external view returns (address);
     function feeToSetter() external view returns (address);
     function feeToAlloc() external view returns (uint);
+    function feeToAllocSetter() external view returns (address);
     function defaultFee() external view returns (uint);
+    function defaultFeeSetter() external view returns (address);
     function FEE_DENOMINATOR() external view returns (uint);
     function getPairFee(address pair) external view returns (uint);
+    function pairFeeSetter() external view returns (address);
 
     function getPair(address tokenA, address tokenB) external view returns (address pair);
     function allPairs(uint) external view returns (address pair);
@@ -26,6 +32,9 @@ interface IGinFinanceFactory {
     function setFeeTo(address) external;
     function setFeeToSetter(address) external;
     function setFeeToAlloc(uint) external;
+    function setFeeToAllocSetter(address) external;
     function setDefaultFee(uint) external;
+    function setDefaultFeeSetter(address) external;
     function setPairFee(address pair, uint fee) external;
+    function setPairFeeSetter(address) external;
 }
