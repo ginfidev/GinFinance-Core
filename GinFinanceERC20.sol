5dd181a001a32382145813e9c6cb04ecf1bb5241
ginfidev <dev@gin.finance>
2022-04-27 23:24:46 -0400

    Update domain separator

--- GinFinanceERC20.sol
+++ GinFinanceERC20.sol
@@ -2,6 +2,7 @@
 
 pragma solidity 0.6.12;
 
+import './libraries/ChainId.sol';
 import './libraries/SafeMath.sol';
 
 contract GinFinanceERC20 {
@@ -9,30 +10,35 @@ contract GinFinanceERC20 {
 
     string public constant name = 'GinFinance LP Token';
     string public constant symbol = 'GLP';
+    string public constant version = '1';
     uint8 public constant decimals = 18;
     uint  public totalSupply;
     mapping(address => uint) public balanceOf;
     mapping(address => mapping(address => uint)) public allowance;
 
-    bytes32 public DOMAIN_SEPARATOR;
     // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
     bytes32 public constant PERMIT_TYPEHASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
     mapping(address => uint) public nonces;
+
+    bytes32 private immutable nameHash;
+    bytes32 private immutable versionHash;
 
     event Approval(address indexed owner, address indexed spender, uint value);
     event Transfer(address indexed from, address indexed to, uint value);
 
     constructor() public {
-        uint chainId;
-        assembly {
-            chainId := chainid()
-        }
-        DOMAIN_SEPARATOR = keccak256(
+        nameHash = keccak256(bytes(name));
+        versionHash = keccak256(bytes(version));
+    }
+
+    function DOMAIN_SEPARATOR() public view returns (bytes32) {
+        return keccak256(
             abi.encode(
-                keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)'),
-                keccak256(bytes(name)),
-                keccak256(bytes('1')),
-                chainId,
+                // keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)')
+                0x8b73c3c69bb8fe3d512ecc4cf759cc79239f7b179b0ffacaa9a75d522b39400f,
+                nameHash,
+                versionHash,
+                ChainId.get(),
                 address(this)
             )
         );
@@ -85,7 +91,7 @@ external {
         bytes32 digest = keccak256(
             abi.encodePacked(
                 '\x19\x01',
-                DOMAIN_SEPARATOR,
+                DOMAIN_SEPARATOR(),
                 keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, nonces[owner]++, deadline))
             )
         );
