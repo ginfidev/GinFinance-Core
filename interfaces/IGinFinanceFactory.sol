7e2d4b4142971139d0db156223670a25bc1f7172
ginfidev <dev@gin.finance>
2022-04-27 23:26:10 -0400

    Add events to factory interface

--- interfaces/IGinFinanceFactory.sol
+++ interfaces/IGinFinanceFactory.sol
@@ -4,6 +4,11 @@ pragma solidity 0.6.12;
 
 interface IGinFinanceFactory {
     event PairCreated(address indexed token0, address indexed token1, address pair, uint);
+    event SetPairFee(address indexed pair, uint pairFee);
+    event SetDefaultFee(uint defaultFee);
+    event SetFeeToAlloc(uint feeToAlloc);
+    event SetFeeToSetter(address feeToSetter);
+    event SetFeeTo(address feeTo);
 
     function feeTo() external view returns (address);
     function feeToSetter() external view returns (address);
